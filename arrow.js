cbbf666d653cb836722b37c88ebee9d64a136947
Demo <sreediyacm@gmail.com>
2026-05-08 11:30:53 +0530

    add example

---
+++ arrow.js
@@ -0,0 +1,20 @@
+const mul=(a,b)=>a*b;
+console.log(mul(5,4)); 
+
+const even=(a)=>a%2==0;
+console.log(even(8))
+
+const sq=(a)=>a*a
+console.log(sq(4))
+
+const person={name:"Diya",age:20}
+const{name,age}=person
+console.log(person); 
+
+const col=["blue","black","red"]
+const[first,second,third]=col
+console.log(first)
+
+const place={city:"farooq",state:"kerala"}
+const{city,state}=place
+console.log(place)
